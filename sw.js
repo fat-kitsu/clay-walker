10613d61efc6c291498a2ce1ea5249b61ecb232a
fat-kitsu <147811379+fat-kitsu@users.noreply.github.com>
2025-11-26 12:14:02 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,54 @@
+// CLAY Walker Service Worker
+const CACHE_NAME = 'clay-walker-v2.2';
+const ASSETS_TO_CACHE = [
+  './',
+  './clay-walker-pwa.html'
+];
+
+// 安裝事件 - 快取資源
+self.addEventListener('install', (event) => {
+  console.log('[SW] Installing...');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('[SW] Caching assets');
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+// 啟動事件 - 清理舊快取
+self.addEventListener('activate', (event) => {
+  console.log('[SW] Activating...');
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('[SW] Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Fetch事件 - 提供快取內容
+self.addEventListener('fetch', (event) => {
+  // 只快取同源請求
+  if (!event.request.url.startsWith(self.location.origin)) {
+    return;
+  }
+  
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
